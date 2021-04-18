5975e8217cec0e9335b8acc72c2a04e3657aa44a
obarker <o.j.barker@hotmail.co.uk>
2021-04-18 22:42:45 +0100

    missile turret added

--- Ollie/New Unity Project/Assets/Scripts/Shop.cs
+++ Ollie/New Unity Project/Assets/Scripts/Shop.cs
@@ -16,10 +16,10 @@ public class Shop : MonoBehaviour {
         buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
     }
 
-    public void PurchaseAnotherTurret()
+    public void PurchaseMissileLauncher()
     {
-        Debug.Log("Another Turret Selected");
-        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+        Debug.Log("Misisle Launcher Selected");
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
     }
 
 
